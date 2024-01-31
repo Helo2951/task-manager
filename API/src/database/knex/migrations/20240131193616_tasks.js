4d3166ad1bf2eb3cc7b11249c597b7fabccf2cec
Helo2951 <heloisa.2951@aluno.pr.senac.br>
2024-01-31 17:00:56 -0300

    criação da migration tasks

---
+++ API/src/database/knex/migrations/20240131193616_tasks.js
@@ -0,0 +1,16 @@
+
+exports.up = (knex) => {
+  return knex.schema.createTable('tasks', (table) => {
+    table.increments('id').primary();
+    table.string('title').notNullable()
+    table.string('content');
+    table.integer('user_id').references('id').inTable('users').onDelete('CASCADE');
+    table.timestamp('created_at').default(knex.fn.now());
+    table.datetime('deadline').notNullable()
+  })
+
+}
+
+exports.down = (knex) => {
+  return knex.schema.dropTableIfExists('tasks')
+};
